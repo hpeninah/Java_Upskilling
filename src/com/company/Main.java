6fa7dd33041b28fb86765f6c52c78864abda77b0
Hannah Mulato <72578792+hpeninah@users.noreply.github.com>
2022-01-04 15:57:37 -0500

    completed all exercises

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,7 +54,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the temperature in Celsius:");
         double celsius = sc.nextDouble();
-        double fahrenheit = (celsius * (9/5)) + 32;
+        double fahrenheit = ((celsius*9)/5)+32;
         System.out.println("The temperature in Fahrenheit is: " + fahrenheit);
     }
 
@@ -62,18 +62,59 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the temperature in Fahrenheit:");
         double fahrenheit = sc.nextDouble();
-        double celsius = (fahrenheit - 32) * (5/9);
+        double celsius = (fahrenheit-32)*5/9;
         System.out.println("The temperature in Celsius is: " + celsius);
     }
 
 
     //Method for Distance Conversion (inch to centimeter)
+    static void InchToCent() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the distance in inches:");
+        double inches = sc.nextDouble();
+        double centimeter = inches * 2.54;
+        System.out.println("The distance in centimeters is: " + centimeter);
+    }
+
+    static void CentToInch() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the distance in centimeters:");
+        double centimeters = sc.nextDouble();
+        double inches = centimeters/2.54;
+        System.out.println("The distance in centimeters is: " + inches);
+    }
 
     public static void main(String[] args) {
-        //TriangleAreaPerimeter();
-        //RectangleAreaPerimeter();
-        //CircleAreaPerimeter();
-        //CelsiusToFahrenheit();
-        FahrenheitToCelsius();
+        int option;
+        Scanner sc = new Scanner(System.in);
+        //Main Menu
+        System.out.println("Choose your option:");
+        System.out.println("1. Calculate Triangle Area and Perimeter");
+        System.out.println("2. Calculate Rectangle Area and Perimeter");
+        System.out.println("3. Calculate Circle Area and Perimeter");
+        System.out.println("4. Celsius to Fahrenheit Conversion");
+        System.out.println("5. Fahrenheit to Celsius Conversion");
+        System.out.println("6. Inches to Centimeter Conversion");
+        System.out.println("7. Centimeter to Inches Conversion");
+        System.out.println("Enter option (1-7): ");
+        option = sc.nextInt();
+
+        if(option == 1){
+            TriangleAreaPerimeter();
+        } else if(option == 2) {
+            RectangleAreaPerimeter();
+        } else if(option == 3) {
+            CircleAreaPerimeter();
+        } else if(option == 4) {
+            CelsiusToFahrenheit();
+        } else if(option == 5) {
+            FahrenheitToCelsius();
+        } else if(option == 6) {
+            InchToCent();
+        } else if(option == 7) {
+            CentToInch();
+        } else {
+            System.out.println("Not an option, please try again.");
+        }
     }
 }
