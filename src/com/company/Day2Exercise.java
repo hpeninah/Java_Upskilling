f1050d6d0b2d0ced16c391696449b5980657a239
Hannah Mulato <72578792+hpeninah@users.noreply.github.com>
2022-01-05 15:42:56 -0500

    re-evaluating program

---
+++ src/com/company/Day2Exercise.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Day2Exercise {
+
+    //Method to find greatest of 3 given numbers using if...else
+    static void GreatestOf3() {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
+
+        if(a >= b && a >= c) {
+            System.out.println(a + " is the greatest number.");
+        }
+        else if(b>=a && b>=c) {
+            System.out.println(b + " is the greatest number.");
+        }
+        else {
+            System.out.println(c + " is the greatest number.");
+        }
+    }
+
+    //Method for while loop with rate of interest in loan and EMI
+    static void LoanEmiInterest() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the loan amount: ");
+        int loan = sc.nextInt();
+        System.out.println("Enter the EMI amount: ");
+        int emi = sc.nextInt();
+        System.out.println("Enter the interest rate: ");
+        int intRate = sc.nextInt();
+        int noemis = 0;
+
+        while( loan > 0 ) {
+            loan -= emi;
+            noemis ++;
+            System.out.println("Paying "+emi+" of the installment "+noemis+"");
+        }
+
+        System.out.println("You are free from the loan after "+noemis+" installments and interest are paid.");
+    }
+
+    //Method to display numbers from 1 to n (even or odd)
+    static void EvenOrOdd() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value you want to count to: ");
+
+        int n = sc.nextInt();
+        System.out.println("Numbers are: ");
+        for(int i=1; i <=n; i++) {
+            if(i%2 == 0) {
+                System.out.println(i + " is even.");
+            }
+            else{
+                System.out.println(i+ " is odd.");
+            }
+        }
+    }
+
+    /*
+
+    display numbers from 1 to n (n is the range given
+    by the user during the execution) and display
+    even/odd next to each number[
+     */
+
+    public static void main(String[] args) {
+        //GreatestOf3();
+        LoanEmiInterest();
+        //EvenOrOdd();
+    }
+}
